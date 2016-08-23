72b8ad1936393affeb05e7d49cbb18b314cd71f0
Federico Brigante <bfred-it@users.noreply.github.com>
2016-08-23 13:53:10 +0700

    Added rollup-plugin-commonjs

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
+import commonJS from 'rollup-plugin-commonjs';
 
 export default {
 	plugins: [
 		nodeResolve({
 			browser: true
-		})
+		}),
+		commonJS()
 	],
 	format: 'iife'
 };
